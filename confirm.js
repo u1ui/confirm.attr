89c25aee64bd0e070b3964bd1fe0b99a26204c64
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-20 20:34:59 +0200

    initial

---
+++ confirm.js
@@ -0,0 +1,56 @@
+
+
+let resentConfirmed = false;
+
+document.addEventListener('submit', async e=>{
+    if (resentConfirmed) return;
+
+    const form = e.target;
+    const btn = e.submitter;
+
+    let element = null;
+    if (form.hasAttribute('u1-confirm')) element = form;
+    if (btn && btn.hasAttribute('u1-confirm')) element = btn;
+
+    if (element) {
+        let msg = getMessage(element);
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        let {confirm} = await import('../dialog.js/dialog.js');
+        let ok = await confirm(msg);
+        if (ok) {
+            resentConfirmed = true;
+            form.requestSubmit(btn);
+            resentConfirmed = false;
+        }
+    }
+
+},true);
+
+/*
+document.addEventListener('click', async e=>{
+    if (resentConfirmed) return;
+    let element = e.target.closest('[u1-confirm]');
+    if (!element) return;
+    if (element.form || element.tagName === 'FORM') {
+        console.log('handled by submit event');
+        return;
+    }
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    let {confirm} = await import('../dialog.js/dialog.js');
+    let msg = getMessage(element);
+    let ok = await confirm(msg);
+    if (ok) {
+        resentConfirmed = true;
+        element.click();
+        resentConfirmed = false;
+    }
+},true);
+*/
+
+function getMessage(el) {
+    return el.getAttribute('u1-confirm') || 'Are you sure?';
+}
